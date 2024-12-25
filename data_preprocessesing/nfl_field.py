23c42e86e4a3a59cb15a9405ee68548f7be0e332
Kush Trivedi <kushtrivedi14728@gmail.com>
2024-12-25 11:31:58 -0600

    Preprocessesing: Data Loading & NFL Field Creation

--- data_preprocessesing/nfl_field.py
+++ data_preprocessesing/nfl_field.py
@@ -161,3 +161,147 @@ class NFLFieldVertical:
 
 
 
+
+# import os
+# import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# from PIL import Image
+# from matplotlib.patches import Rectangle
+
+
+# plt.rcParams['figure.dpi'] = 180
+# plt.rcParams["figure.figsize"] = (25, 17)
+# colors = sns.color_palette('Set3')
+# sns.set_theme(rc={
+#     'axes.facecolor': '#FFFFFF',
+#     'figure.facecolor': '#FFFFFF',
+#     'font.sans-serif': 'Arial',
+#     'font.family': 'sans-serif'
+# })
+
+
+# class NFLFieldVertical:
+#     def __init__(self, width=53.3, height=120, home_team="", home_team_color="", visitor_team = "", visitor_team_color="",logo_abbr=""):
+#         self.width = width
+#         self.height = height
+#         self.home_team = home_team
+#         self.home_team_color = home_team_color
+#         self.visitor_team = visitor_team
+#         self.visitor_team_color = visitor_team_color
+#         self.team_names = {
+#             'LA': 'Los Angeles Rams',
+#             'ATL': 'Atlanta Falcons',
+#             'CAR': 'Carolina Panthers',
+#             'CHI': 'Chicago Bears',
+#             'CIN': 'Cincinnati Bengals',
+#             'DET': 'Detroit Lions',
+#             'HOU': 'Houston Texans',
+#             'MIA': 'Miami Dolphins',
+#             'NYJ': 'New York Jets',
+#             'WAS': 'Washington Commanders',
+#             'ARI': 'Arizona Cardinals',
+#             'LAC': 'Los Angeles Chargers',
+#             'MIN': 'Minnesota Vikings',
+#             'TEN': 'Tennessee Titans',
+#             'DAL': 'Dallas Cowboys',
+#             'SEA': 'Seattle Seahawks',
+#             'KC': 'Kansas City Chiefs',
+#             'BAL': 'Baltimore Ravens',
+#             'CLE': 'Cleveland Browns',
+#             'JAX': 'Jacksonville Jaguars',
+#             'NO': 'New Orleans Saints',
+#             'NYG': 'New York Giants',
+#             'PIT': 'Pittsburgh Steelers',
+#             'SF': 'San Francisco 49ers',
+#             'DEN': 'Denver Broncos',
+#             'LV': 'Las Vegas Raiders',
+#             'GB': 'Green Bay Packers',
+#             'BUF': 'Buffalo Bills',
+#             'PHI': 'Philadelphia Eagles',
+#             'IND': 'Indianapolis Colts',
+#             'NE': 'New England Patriots',
+#             'TB': 'Tampa Bay Buccaneers'
+#         }
+#         self.fig, self.ax = self.create_pitch()
+
+#     def create_pitch(self):
+#         fig, ax = plt.subplots()
+#         ax.set_xlim(0, self.width)  
+#         ax.set_ylim(0, self.height - 1) 
+#         ax.axis('off')
+
+#         background = Rectangle((0, 0), self.width, self.height, linewidth=1, facecolor='#97BC62FF', edgecolor='black', capstyle='round')
+#         ax.add_patch(background)
+
+#         for i in range(21):
+#             if i % 2 != 0:
+#                 ax.plot([0, self.width], [10 + 5 * i] * 2, c="white", linestyle='--', lw=1, alpha=0.4)
+#             else:
+#                 ax.plot([0, self.width], [10 + 5 * i] * 2, c="white", lw=1, alpha=0.8)
+
+#         for units in range(10, 100, 10):
+#             units_text = units if units <= 50 else 100 - units
+#             ax.text(self.width - 7.5, 10 + units - 1.1, units_text, size=18, c="white", weight="bold", alpha=0.8)
+#             ax.text(7.5, 10 + units - 1.1, units_text, size=18, c="white", weight="bold", alpha=0.8)
+
+#         for x in range(20):
+#             for j in range(1, 5):
+#                 ax.plot([1, 3], [10 + x * 5 + j] * 2, color="white", lw=1, alpha=0.8)
+#                 ax.plot([self.width - 1, self.width - 3], [10 + x * 5 + j] * 2, color="white", lw=1, alpha=0.8)
+ 
+#         gap = 5  
+#         center_x1 = (self.width / 2) - gap  
+#         center_x2 = (self.width / 2) + gap 
+#         for y in range(20):
+#             for j in range(1, 5):
+#                 ax.plot([center_x1, center_x1 + 1], [10 + y * 5 + j] * 2, color="white", lw=1, alpha=0.8)
+#                 ax.plot([center_x2, center_x2 - 1], [10 + y * 5 + j] * 2, color="white", lw=1, alpha=0.8)
+
+#         visitor_full_name = self.team_names.get(self.visitor_team, self.visitor_team)  # Get full name
+#         ax.text(self.width / 2, 5.5, self.visitor_team, size=30, c="white", weight="bold", ha='center')  
+#         ax.text(self.width / 2, 1, visitor_full_name, size=30, c="white", weight="bold", ha='center')  
+
+#         end_zone_top = Rectangle((0, 0), self.width, 10, ec="black", fc=self.visitor_team_color, lw=1)
+#         ax.add_patch(end_zone_top)
+
+#         home_full_name = self.team_names.get(self.home_team, self.home_team)  # Get full name
+#         ax.text(self.width / 2, self.height - 5, self.home_team, size=30, c="white", weight="bold", ha='center')  
+#         ax.text(self.width / 2, self.height - 9, home_full_name, size=30, c="white", weight="bold", ha='center')  
+
+#         end_zone_bottom = Rectangle((0, self.height - 10), self.width, 10, ec="black", fc=self.home_team_color, lw=1)
+#         ax.add_patch(end_zone_bottom)
+
+#         logo_path = os.path.join('assets/logo', f'{self.home_team}.png')
+#         if os.path.exists(logo_path):
+#             logo = Image.open(logo_path)
+#             logo = logo.rotate(90, expand=True)
+#             width, height = logo.size
+#             square_size = max(width, height)
+#             square_logo = Image.new('RGBA', (square_size, square_size), (255, 255, 255, 0))
+#             paste_x = (square_size - width) // 2
+#             paste_y = (square_size - height) // 2
+#             square_logo.paste(logo, (paste_x, paste_y))
+            
+#             logo_array = np.array(square_logo)
+#             logo_size = 10  
+#             logo_extent = [
+#                 (self.width - logo_size) / 2,
+#                 (self.width + logo_size) / 2,
+#                 (self.height - logo_size) / 2,
+#                 (self.height + logo_size) / 2
+#             ]
+#             ax.imshow(logo_array, extent=logo_extent, aspect='auto', zorder=10)
+#         else:
+#             print(f"Logo file not found at {logo_path}")
+
+
+#         return fig, ax
+
+#     def save_pitch(self, folder_path, filename='pitch.png'):
+#         if not os.path.exists(folder_path):
+#             os.makedirs(folder_path)
+
+#         file_path = os.path.join(folder_path, filename)
+#         self.fig.savefig(file_path, bbox_inches='tight')
+#         plt.close(self.fig)
